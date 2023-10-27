0b194464302a7eec924ffd03ba55d9db5e1ad235
PeterHellmuth <10523853+PeterHellmuth@users.noreply.github.com>
2023-10-27 13:12:54 -0500

    Add helmet and rate-limiter

--- app.js
+++ app.js
@@ -21,6 +21,29 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
+// Create the Express application object
+const app = express();
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+// Add helmet to the middleware chain.
+// Set CSP headers to allow our Bootstrap and Jquery to be served
+const helmet = require("helmet");
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  })
+);
+
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
